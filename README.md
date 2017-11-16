328f803712c86b384b9b9c28d6388f7f0f5438ef
Yair Aronshtam (yaronsht) <yair.ar@gmail.com>
2017-11-16 19:03:09 +0200

    version 1.5

---
+++ README.md
@@ -0,0 +1,1 @@
+# deadseastory.com
---
+++ js/timeline_json.js
@@ -0,0 +1,61 @@
+var timeline_json = 
+{
+    "title": {
+        "media": {
+          "url": "img/subscribe-bg.jpg",
+          "caption": "Dead Sea",
+          "credit": "photo:<a href='#'>Noam Bedein</a>"
+        },
+        "text": {
+          "headline": "Dead sea time lapse<br/> 2016 - 2017",
+          "text": "<p>Dead sea time lapse</p>"
+        }
+    },
+    "events": [
+      {
+        "media": {
+          "url": "img/timeline1.jpg",
+          "caption": "Dead sea",
+          "credit": "photo:<a href='#'>Noam Bedein</a>"
+        },
+        "start_date": {
+          "month": "4",
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Exploration begins",
+          "text": "<p>In the 20th century the water level of the Dead Sea went down over 35 meters and today it is minus 420 meters below sea level</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "img/timeline2.jpg",
+          "caption": "Dead sea",
+          "credit": "photo:<a href='#'>Noam Bedein</a>"
+        },
+        "start_date": {
+          "month": "1",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "10 months later",
+          "text": "<p>Scientist are saying that by the year 2050, only a little pool of salt will remain</p>"
+        }
+      },      
+      {
+        "media": {
+          "url": "https://youtu.be/MMhCzA5d5rA",
+          "caption": "The Dead Sea’s Treasures Exploration Boat ride",
+          "credit": ""
+        },
+        "start_date": {
+          "month": "2",
+          "year": "2017"
+        },
+        "text": {
+          "headline": "Exploration Boat ride",
+          "text": "<p>I continue to take delegations from Israel and abroad on a unique awareness boat ride, where we will explore new geological phenomena and measure the water level. We can reach areas that are only accessible by this boat, and therefore, I can show these wonders to the world in ways never done before.</p>"
+        }
+      },
+     ]
+};        
